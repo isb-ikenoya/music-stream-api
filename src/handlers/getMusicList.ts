8c97d6a966c5b5938034d211e2062a9368d2a803
yuta_ikenoya <yuta_ikenoya@isb.co.jp>
2026-06-05 13:43:23 +0900

    first-commit

---
+++ src/handlers/getMusicList.ts
@@ -0,0 +1,18 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      message: "OK",
+    }),
+  };
+};
